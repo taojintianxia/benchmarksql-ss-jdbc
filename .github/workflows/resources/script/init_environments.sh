9eb69a6498aa64065fb47fee1eaf7f32ea9e3869
孙念君 Nianjun Sun <sunnianjun@apache.org>
2022-10-24 14:57:15 +0800

    trigger action

--- .github/workflows/resources/script/init_environments.sh
+++ .github/workflows/resources/script/init_environments.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 input="../env/.env"
 
-while read -r line
-do
-  echo "$line" >> $GITHUB_ENV
-done < "$input"
+function init_environments_variables_to_git {
+  echo `pwd`
+  while read -r line
+  do
+    echo "$line" >> $GITHUB_ENV
+  done < "$input"
+}
+
+init_environments_variables_to_git
